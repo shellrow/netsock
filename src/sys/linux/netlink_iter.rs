0f6129155195846a4532a8e98f857a149a50dede
shellrow <81893184+shellrow@users.noreply.github.com>
2024-12-31 14:41:06 +0000

    Use libc::tcp_info instead of ffi tcp_info

--- src/sys/linux/netlink_iter.rs
+++ src/sys/linux/netlink_iter.rs
@@ -6,6 +6,7 @@ use libc::*;
 use std;
 use std::io;
 use std::mem::size_of;
+use std::ptr::read_unaligned;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 #[cfg(target_env = "musl")]
@@ -190,8 +191,8 @@ unsafe fn parse_tcp_state(diag_msg: &inet_diag_msg, rtalen: usize) -> TcpState {
     let mut attr = (diag_msg as *const inet_diag_msg).offset(1) as *const rtattr;
     while RTA_OK!(attr, len) {
         if (&*attr).rta_type == INET_DIAG_INFO as u16 {
-            let tcpi = &*(RTA_DATA!(attr) as *const tcp_info);
-            return TcpState::from(tcpi.state);
+            let tcpi = read_unaligned(RTA_DATA!(attr) as *const tcp_info);
+            return TcpState::from(tcpi.tcpi_state);
         }
         attr = RTA_NEXT!(attr, len);
     }
