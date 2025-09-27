f221798fedaff4f416574028d3928c4a147ad163
shellrow <shellrow@fortnium.com>
2025-09-27 15:38:43 +0900

    Add SocketQuery

--- src/socket.rs
+++ src/socket.rs
@@ -1,3 +1,4 @@
+use crate::error::Error;
 use crate::process::Process;
 use crate::state::TcpState;
 use std::net::IpAddr;
@@ -75,5 +76,146 @@ impl SocketInfo {
             ProtocolSocketInfo::Tcp(s) => s.local_port,
             ProtocolSocketInfo::Udp(s) => s.local_port,
         }
+    }
+
+    /// Retrieves the remote IP address associated with the socket when applicable.
+    pub fn remote_addr(&self) -> Option<IpAddr> {
+        match &self.protocol_socket_info {
+            ProtocolSocketInfo::Tcp(s) => Some(s.remote_addr),
+            ProtocolSocketInfo::Udp(_) => None,
+        }
+    }
+
+    /// Retrieves the remote port associated with the socket when applicable.
+    pub fn remote_port(&self) -> Option<u16> {
+        match &self.protocol_socket_info {
+            ProtocolSocketInfo::Tcp(s) => Some(s.remote_port),
+            ProtocolSocketInfo::Udp(_) => None,
+        }
+    }
+
+    /// Returns true if any associated process matches the provided PID.
+    pub fn is_owned_by_pid(&self, pid: u32) -> bool {
+        self.processes.iter().any(|process| process.pid == pid)
+    }
+}
+
+/// Describes criteria that can be used to filter [`SocketInfo`] values.
+#[derive(Clone, Debug, Default)]
+pub struct SocketQuery {
+    local_addr: Option<IpAddr>,
+    local_port: Option<u16>,
+    remote_addr: Option<IpAddr>,
+    remote_port: Option<u16>,
+    owner_pid: Option<u32>,
+}
+
+impl SocketQuery {
+    /// Creates an empty query that matches all sockets.
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Filters sockets to those bound to the specified local IP address.
+    pub fn with_local_addr(mut self, addr: IpAddr) -> Self {
+        self.local_addr = Some(addr);
+        self
+    }
+
+    /// Filters sockets to those bound to the specified local port.
+    pub fn with_local_port(mut self, port: u16) -> Self {
+        self.local_port = Some(port);
+        self
+    }
+
+    /// Filters sockets to those connected to the specified remote IP address.
+    pub fn with_remote_addr(mut self, addr: IpAddr) -> Self {
+        self.remote_addr = Some(addr);
+        self
+    }
+
+    /// Filters sockets to those connected to the specified remote port.
+    pub fn with_remote_port(mut self, port: u16) -> Self {
+        self.remote_port = Some(port);
+        self
+    }
+
+    /// Filters sockets to those owned by the specified PID.
+    pub fn with_owner_pid(mut self, pid: u32) -> Self {
+        self.owner_pid = Some(pid);
+        self
+    }
+
+    /// Returns true if the socket satisfies the query criteria.
+    pub fn matches(&self, socket: &SocketInfo) -> bool {
+        if let Some(addr) = self.local_addr {
+            if socket.local_addr() != addr {
+                return false;
+            }
+        }
+
+        if let Some(port) = self.local_port {
+            if socket.local_port() != port {
+                return false;
+            }
+        }
+
+        if let Some(addr) = self.remote_addr {
+            if socket.remote_addr() != Some(addr) {
+                return false;
+            }
+        }
+
+        if let Some(port) = self.remote_port {
+            if socket.remote_port() != Some(port) {
+                return false;
+            }
+        }
+
+        if let Some(pid) = self.owner_pid {
+            if !socket.is_owned_by_pid(pid) {
+                return false;
+            }
+        }
+
+        true
+    }
+}
+
+/// Provides convenient filtering helpers for iterators that yield [`SocketInfo`] values.
+pub trait SocketIteratorExt: Iterator<Item = Result<SocketInfo, Error>> + Sized {
+    /// Filters sockets using the provided [`SocketQuery`].
+    fn filter_by_query(self, query: SocketQuery) -> FilterByQuery<Self> {
+        FilterByQuery { inner: self, query }
+    }
+}
+
+impl<I> SocketIteratorExt for I where I: Iterator<Item = Result<SocketInfo, Error>> + Sized {}
+
+/// Iterator returned by [`SocketIteratorExt::filter_by_query`].
+pub struct FilterByQuery<I> {
+    inner: I,
+    query: SocketQuery,
+}
+
+impl<I> Iterator for FilterByQuery<I>
+where
+    I: Iterator<Item = Result<SocketInfo, Error>>,
+{
+    type Item = Result<SocketInfo, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(item) = self.inner.next() {
+            match item {
+                Ok(socket) => {
+                    if self.query.matches(&socket) {
+                        return Some(Ok(socket));
+                    }
+                }
+                Err(err) => return Some(Err(err)),
+            }
+        }
+
+        None
     }
 }
